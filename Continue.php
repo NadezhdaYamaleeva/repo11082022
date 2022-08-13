36c28e7ea82d9c0029bb0f5cce62354151ca5bc8
unknown <Niromedium@gmail.com>
2022-08-13 23:15:31 +0500

    Finish work this mobail

--- Continue.php
+++ Continue.php
@@ -1,1 +1,10 @@
 I`m write here working site code
+To have this happen automatically for branches without a tracking
+upstream, see 'push.autoSetupRemote' in 'git help config'.
+
+The authenticity of host 'github.com (140.82.121.4)' can't be established.
+ED25519 key fingerprint is SHA256:+DiY3wvvV6TuJJhbpZisF/zLDA0zPMSvHdkr4UvCOqU.
+This key is not known by any other names
+Warning: Permanently added 'github.com' (ED25519) to the list of known hosts.
+Enumerating objects: 3, done.
+Counting objects: 100% (3/3), done.
